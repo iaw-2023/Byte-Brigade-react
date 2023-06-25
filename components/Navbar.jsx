b988cf0f862819575213b98627626a40115d54eb
ssunburst <fabrizio.m@live.com.ar>
2023-06-25 00:53:13 -0300

    Ahora sí corregido el bendito bug

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -33,13 +33,13 @@ const Navbar = () => {
     }
 
     return (
-        <nav id="nav" className="bg-gray-950 flex justify-start items-baseline p-4 w-full mb-8 gap-4 border-0 border-b-2 border-gray-400">
+        <nav id="nav" className="bg-gray-950 flex justify-start items-baseline p-4 w-screen mb-8 gap-4 border-0 border-b-2 border-gray-400">
             <Link href='/'>
                 <h1 className="text-gray-200 min-w-max hover:text-red-200 font-serif text-4xl ml-1">
                     El Corchazo
                 </h1>
             </Link>
-            <div className="flex uppercase w-full justify-end visible xl:hidden text-gray-200 text-3xl font-thin hover:text-red-200 px-2 py-1">
+            <div className="flex uppercase w-full justify-end visible xl:hidden text-gray-200 text-lg xs:text-xl font-thin hover:text-red-200 pr-1 py-1">
                     <Link href="/categorias">Categorías</Link>
             </div>
             <div className="hidden grow xl:flex xl:justify-end font-thin text-center text-gray-200 gap-12">
