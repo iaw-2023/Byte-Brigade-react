563f5e4819e5f11013064a4e0306e083a5fa0e95
ssunburst <fabrizio.m@live.com.ar>
2024-01-11 18:14:29 -0300

    TESTING

--- app/articulos/page.js
+++ app/articulos/page.js
@@ -1,80 +1,32 @@
-'use client';
+import { fetchArticles } from '../lib/data';
+import ArticleList from '../ui/articulos/ArticleList';
 
-import requests from '@/app/lib/requests';
-import {useEffect, useState} from 'react';
-import { useSearchParams } from 'next/navigation'
-import { getNotFound, getSearching } from '@/app/lib/texts';
-import axios from 'axios';
-import TopicTag from '@/app/ui/TopicTag';
-import Link from 'next/link';
+async function Page ({ searchParams }) {
+    const {author, topic, page} = searchParams;
+    const articles = await fetchArticles(page, author, topic);
 
-function Page () {
-    const [articles, setArticles] = useState([]);
-    const [searching, setSearching] = useState(true);
-    const searchParams = useSearchParams();
+    function handlePrevClick() {
+        const params = new URLSearchParams(searchParams);
+        const currentPage = Number(params.get('page'));
+        params.set('page', currentPage - 1);
+        // router.replace(`/articulos/${params.toString()}`);
+    }
 
-    const fetchArticles = async () => {
-            let url = `${requests.articles.index}?`;
-            const author = searchParams.get('author');
-            const topic = searchParams.get('topic');
-
-            if (author) {
-                url = `${url}author=${author}`;
-                if (topic) {
-                    url = `${url}&topic=${topic}`
-                }
-            }
-            else if (topic) {
-                url = `${url}topic=${topic}`;
-            }
-
-            try {
-                console.log(url);
-                const response = await axios.get(url);
-                setArticles(response.data.data);
-                setSearching(false);
-            } catch (error) {
-                setSearching(false);
-            }
-    };
-
-    useEffect(() => {        
-        fetchArticles();
-    }, []);
-
-    useEffect(() => {        
-        fetchArticles();
-    }, [searchParams]);
-
+    function handleNextClick() {
+        const params = new URLSearchParams(searchParams);
+        const currentPage = Number(params.get('page'));
+        params.set('page', currentPage - 1);
+        // router.replace(`/articulos/${params.toString()}`);
+    }
 
     return (
             <>
                 <h2 className="text-5xl font-extralight text-gray-900 p-2">
-                        Resultados de búsqueda
+                    {author || topic? "Resultado de búsqueda" : "Todos los artículos"}
                 </h2>
-                <div className="flex flex-col justify-start lg:w-3/4 p-4 gap-4">
-                    
-                    {
-                        (searching || articles.length == 0) && (
-                            <p className="text-2xl text-gray-900">
-                                { searching? getSearching() : getNotFound() }
-                            </p>
-                        )
-                    }
-                    <div className="flex flex-col gap-1 divide-y w-full justify-center">
-                    {
-                        articles.map(article => {
-                            return (
-                                <Link key={article.id} href={`/articulos/${article.id}`}>
-                                    <div className="flex flex-col gap-2 my-4">
-                                        <div className="text-sm"><TopicTag topic={article.topic} /></div>
-                                        <p className="text-xl text-gray-900">{article.title}</p>
-                                    </div>
-                                </Link>
-                            );
-                        })
-                    }
-                    </div>
+                <ArticleList key={page + author + topic} articles={articles} />
+                <div>
+                    BOTONES
                 </div>
             </>
     );
--- app/lib/data.js
+++ app/lib/data.js
@@ -1,6 +1,6 @@
 import requests from "./requests";
 import axios from "axios";
 
-export async function fetchArticles() {
+export async function fetchArticles(page = 1, author = null, topic = null) {
     
 }
