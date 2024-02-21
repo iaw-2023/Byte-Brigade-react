df770ce8aaea3765a94a2ccbfebb020dfaad4c36
ssunburst <fabrizio.m@live.com.ar>
2024-02-21 15:39:38 -0300

    Agregado pero no probado Card Brick

---
+++ app/ui/components/CardBrick.jsx
@@ -0,0 +1,56 @@
+
+import { initMercadoPago } from '@mercadopago/sdk-react';
+import { CardPayment } from '@mercadopago/sdk-react';
+
+initMercadoPago('YOUR_PUBLIC_KEY');
+
+
+const initialization = {
+    amount: 100,
+};
+
+
+const onSubmit = async (formData) => {
+    // callback llamado al hacer clic en el botón enviar datos
+    return new Promise((resolve, reject) => {
+        fetch('/process_payment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+            .then((response) => response.json())
+            .then((response) => {
+                // recibir el resultado del pago
+                resolve();
+            })
+            .catch((error) => {
+                // manejar la respuesta de error al intentar crear el pago
+                reject();
+            });
+    });
+};
+
+
+const onError = async (error) => {
+    // callback llamado para todos los casos de error de Brick
+    console.log(error);
+};
+
+
+const onReady = async () => {
+    /*
+      Callback llamado cuando Brick está listo.
+      Aquí puedes ocultar cargamentos de su sitio, por ejemplo.
+    */
+};
+
+export function CardBrick() {
+    return <CardPayment
+        initialization={initialization}
+        onSubmit={onSubmit}
+        onReady={onReady}
+        onError={onError}
+    />;
+}
