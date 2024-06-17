e7ff5f5b678a3bcfd7194c4e517d161bf1c3f996
ssunburst <fabrizio.m@live.com.ar>
2024-06-17 02:04:37 -0300

    Primer run de navegación offline?????

---
+++ app/~offline/page.js
@@ -0,0 +1,10 @@
+export default function Page() {
+    <div className="flex flex-col h-96 items-center justify-center">
+        <p className="text-xl uppercase font-bold mb-2">
+            No hay conexión a internet
+        </p>
+        <p className="text-center">
+            Pagá la factura y volvé a intentarlo.
+        </p>
+    </div>
+}
